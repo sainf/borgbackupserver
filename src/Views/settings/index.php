e9b1b9bf8681b1ccb9f03aef60c66195a6d4f149
Marc Pope <marcpope@me.com>
2026-02-03 11:04:13 -0500

    Shorten S3 Backups and Borg Version tab labels on mobile

--- src/Views/settings/index.php
+++ src/Views/settings/index.php
@@ -23,7 +23,7 @@ $updateAvailable = $updateService->isUpdateAvailable();
     </li>
     <li class="nav-item">
         <a class="nav-link <?= $activeTab === 'offsite' ? 'active' : '' ?>" href="/settings?tab=offsite">
-            <i class="bi bi-bucket me-1"></i><span class="tab-label">S3 Backups</span>
+            <i class="bi bi-bucket me-1"></i><span class="tab-label d-none d-sm-inline">S3 Backups</span><span class="tab-label d-inline d-sm-none">S3</span>
         </a>
     </li>
     <li class="nav-item">
@@ -36,7 +36,7 @@ $updateAvailable = $updateService->isUpdateAvailable();
     </li>
     <li class="nav-item">
         <a class="nav-link <?= $activeTab === 'borg' ? 'active' : '' ?>" href="/settings?tab=borg">
-            <i class="bi bi-box-seam me-1"></i><span class="tab-label">Borg Version</span>
+            <i class="bi bi-box-seam me-1"></i><span class="tab-label d-none d-sm-inline">Borg Version</span><span class="tab-label d-inline d-sm-none">Borg</span>
         </a>
     </li>
 </ul>
